fa33941a2066e850b9f0e5ecf2f80762fb2ecc66
Chris <chris@echoz.io>
2025-10-07 12:09:28 +0200

    feat: add some basic git config

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -10,5 +10,11 @@
     enable = true;
     userName = lib.toSentenceCase user;
     userEmail = email;
+
+    extraConfig = {
+      push.autoSetupRemote = true;
+      pull.rebase = true;
+      init.defaultBranch = "main";
+    };
   };
 }
