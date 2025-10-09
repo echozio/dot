4b630a42ef1583f19fc758abf50ff788508f6285
Chris <chris@echoz.io>
2025-10-09 02:19:41 +0200

    feat: add some key bindings to glirc

--- modules/glirc/default.nix
+++ modules/glirc/default.nix
@@ -26,6 +26,22 @@
           xdg.configFile."glirc/config" = lib.mkIf (cfg.extraConfig != null) {
             text = cfg.extraConfig;
           };
+
+          programs.glirc.extraConfig = lib.mkBefore ''
+            key-bindings:
+              * bind: "C-d"
+                action: scroll-down-small
+              * bind: "C-u"
+                action: scroll-up-small
+              * bind: "C-f"
+                action: scroll-down
+              * bind: "C-b"
+                action: scroll-up
+              * bind: "C-a"
+                action: jump-to-activity
+              * bind: "C-s"
+                action: jump-to-previous
+          '';
         };
     };
 }
