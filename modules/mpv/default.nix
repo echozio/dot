1122c7fb5fb449446c3706787661a37f24f661d1
Chris <chris@echoz.io>
2025-10-12 23:19:04 +0200

    feat: keep mpv open

--- modules/mpv/default.nix
+++ modules/mpv/default.nix
@@ -7,6 +7,11 @@
 {
   home-manager.users.${user}.programs.mpv = {
     enable = true;
+
+    config = {
+      keep-open = true;
+    };
+
     scripts = with pkgs.mpvScripts; [
       uosc
       thumbfast
