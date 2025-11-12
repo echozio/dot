1aa0ec4b07fa1876e5f7a910caba842b18b57145
Chris <chris@echoz.io>
2025-11-12 09:22:34 +0100

    feat: properly configure network for ws

--- hosts/ws/default.nix
+++ hosts/ws/default.nix
@@ -18,6 +18,31 @@
   networking = {
     hostName = "ws";
     hostId = "e27df163";
+
+    useDHCP = false;
+    enableIPv6 = false;
+
+    bridges.br0.interfaces = [
+      "eno1"
+      "eno2"
+    ];
+
+    nameservers = [ "10.120.120.101" ];
+    domain = "lan.inl1.echoz.io";
+    search = [ "lan.inl1.echoz.io" ];
+
+    defaultGateway = "10.210.120.201";
+
+    interfaces.br0.ipv4.addresses = [
+      {
+        address = "10.210.100.101";
+        prefixLength = 16;
+      }
+      {
+        address = "192.168.0.5";
+        prefixLength = 24;
+      }
+    ];
   };
 
   boot = {
