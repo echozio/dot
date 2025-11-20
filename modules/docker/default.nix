0585f88a7daadc2ef20813fd384c90b90e6b0678
Chris <chris@echoz.io>
2025-11-20 12:57:27 +0100

    fix(docker): load iptable_nat needed by some containers

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -4,9 +4,12 @@
     setSocketVariable = true;
   };
 
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_unprivileged_port_start" = 0;
-    "kernel.keys.maxkeys" = 1000000;
-    "kernel.keys.maxbytes" = 25000000;
+  boot = {
+    kernel.sysctl = {
+      "net.ipv4.ip_unprivileged_port_start" = 0;
+      "kernel.keys.maxkeys" = 1000000;
+      "kernel.keys.maxbytes" = 25000000;
+    };
+    kernelModules = [ "iptable_nat" ];
   };
 }
