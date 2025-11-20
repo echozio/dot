013fe8bd29f6788ac3a1e6e60c1355e55ee162f2
Chris <chris@echoz.io>
2025-11-20 12:16:43 +0100

    fix(taskwarrior): pin news.version to the package version

--- modules/taskwarrior/default.nix
+++ modules/taskwarrior/default.nix
@@ -1,15 +1,15 @@
 { user, pkgs, ... }:
 {
   home-manager.users.${user} =
-  { config, ... }:
-  {
-    programs.taskwarrior = {
-      enable = true;
-      package = pkgs.taskwarrior3;
-      dataLocation = "${config.home.homeDirectory}/etc/task";
-      config = {
-        "news.version" = pkgs.taskwarrior3.version;
+    { config, ... }:
+    {
+      programs.taskwarrior = {
+        enable = true;
+        package = pkgs.taskwarrior3;
+        dataLocation = "${config.home.homeDirectory}/etc/task";
+        config = {
+          "news.version" = pkgs.taskwarrior3.version;
+        };
       };
     };
-  };
 }
