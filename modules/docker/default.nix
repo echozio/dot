1808213c8dcee6febc82828b613c75c63993fa64
Chris <chris@echoz.io>
2025-11-20 13:57:03 +0100

    fix(docker): allow ipv4 forwarding

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -7,6 +7,7 @@
   boot = {
     kernel.sysctl = {
       "net.ipv4.ip_unprivileged_port_start" = 0;
+      "net.ipv4.ip_forward" = 1;
       "kernel.keys.maxkeys" = 1000000;
       "kernel.keys.maxbytes" = 25000000;
     };
