9fae88ca74246eaae2f2c4072d5a816d03f93ef2
Chris <chris@echoz.io>
2025-11-21 11:21:37 +0100

    revert: remove kernel keyring sysctls as docker properly releases its keys

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -8,8 +8,6 @@
     kernel.sysctl = {
       "net.ipv4.ip_unprivileged_port_start" = 0;
       "net.ipv4.ip_forward" = 1;
-      "kernel.keys.maxkeys" = 1000000;
-      "kernel.keys.maxbytes" = 25000000;
     };
     kernelModules = [ "iptable_nat" ];
   };
