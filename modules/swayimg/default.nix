3dab7db0ac56a8cdda97d8753aa9a1e282726540
Chris <chris@echoz.io>
2025-11-30 14:57:33 +0100

    feat(swayimg): add keys for copy as path and copy as png image data

--- modules/swayimg/default.nix
+++ modules/swayimg/default.nix
@@ -1,4 +1,9 @@
-{ user, ... }:
+{
+  lib,
+  pkgs,
+  user,
+  ...
+}:
 {
   home-manager.users.${user}.programs.swayimg = {
     enable = true;
@@ -7,6 +12,10 @@
         MouseRight = "mode gallery";
         ScrollUp = "zoom +10";
         ScrollDown = "zoom -10";
+        y = ''exec ${lib.getExe' pkgs.wl-clipboard "wl-copy"} -- "%"'';
+        "Shift+y" = ''exec ${pkgs.writeShellScript "wl-copy-image" ''
+          ${lib.getExe pkgs.imagemagick} - png:- | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}
+        ''} < "%" '';
       };
     };
   };
