6f7d597f685f2ae4520ec13e8bf3ee869c752eab
Chris <chris@echoz.io>
2025-12-01 20:35:45 +0100

    feat(tp): enable tlp

--- hosts/tp/default.nix
+++ hosts/tp/default.nix
@@ -42,4 +42,13 @@
       ];
     };
   };
+
+  powerManagement.powertop.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
 }
