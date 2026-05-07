7a011ed62aad3d0b38b44178a446411c8935116e
Chris <chris@echoz.io>
2026-05-07 17:11:38 +0200

    refactor: remove custom nom options in favor of upstream home-manager options

--- modules/nom/default.nix
+++ modules/nom/default.nix
@@ -1,48 +1,11 @@
+{ user, ... }:
 {
-  lib,
-  pkgs,
-
-  user,
-  ...
-}:
-{
-  home-manager.users.${user} =
-    { config, ... }:
-    {
-      options.programs.nom = {
-        settings = lib.mkOption {
-          type =
-            with lib.types;
-            let
-              jsonValue = nullOr (oneOf [
-                (attrsOf jsonValue)
-                (listOf jsonValue)
-                number
-                str
-                bool
-              ]);
-            in
-            jsonValue;
-          default = null;
-        };
-      };
-
-      config =
-        let
-          cfg = config.programs.nom;
-        in
-        {
-          home.packages = [ pkgs.nom ];
-
-          xdg.configFile."nom/config.yml" = lib.mkIf (cfg.settings != null) {
-            text = builtins.toJSON cfg.settings;
-          };
-
-          programs.nom.settings = {
-            database = lib.mkDefault "../../etc/.nom.db";
-            ordering = lib.mkDefault "desc";
-            refreshInterval = 1;
-          };
-        };
+  home-manager.users.${user}.programs.nom = {
+    enable = true;
+    settings = {
+      database = "../../etc/.nom.db";
+      ordering = "desc";
+      refreshInterval = 1;
     };
+  };
 }
